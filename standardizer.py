a197d166cf7a3f45e8ff819168b02647f5985bc5
Markus O <orpana.markus@gmail.com>
2018-11-06 12:56:05 +0200

    simplified standardizer methods

--- standardizer.py
+++ standardizer.py
@@ -9,8 +9,24 @@ class Standardizer:
         
         assert(method in ['none', 'normal', 'bounded']), "Provide method as either 'none', 'normal', 'midrange' using kwarg 'method'"
         self.method = method
-        self.get_params = getattr(self, self.method+'_params')
+        
+        # Methods for each type of standardization
+        # 1. Empty dummy for no normalization
+        
+        self.none = lambda X, params: X
+        self.none_params = lambda X: [None, None]
+        
+        # 2. With mean and standard deviation
+        self.normal = normal
+        self.normal_params = normal_params
+        
+        # 3. With midrange and range
+        self.bounded = bounded
+        self.bounded_params = bounded_params
+        
+        # Setting standardization method
         self.normalize = getattr(self, self.method)
+        self.get_params = getattr(self, self.method+'_params')
         
     def train(self, X):
 
@@ -22,30 +38,3 @@ class Standardizer:
     def evaluate(self, X):
         
         return self.normalize(X, self.params)
-    
-    # Methods for each type of standardization
-    # 1. Empty dummy for no normalization
-    
-    def none(self, X, params):
-
-        return X
-
-    def none_params(self, X):
-
-        return [None,None]
-    
-    # 2. With mean and standard deviation
-    
-    def normal(self, X, params):
-        return normal(X, params)
-
-    def normal_params(self, X):
-        return normal_params(X)
-
-    # 3. With midrange and range
-    
-    def bounded(self, X, params):
-        return bounded(X, params)
-
-    def bounded_params(self, X):
-        return bounded_params(X)
