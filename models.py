categorical grad check fine; l2 regression bug fixed -- missing 1/2 coefficient in loss; l1 regression bug fixed -- missing correct derivative for output layer i.e. np.sign(A - Y) in sequential model; general bug fixed -- epsilon too large resulting in larger error --> changed from 1e-5 to 1e-7; to do: regularization

models.py
@@ -74,7 +74,10 @@ class Sequential:
 		
 		layers = self.layers
 		
-		dZ = A - Y
+		if self.params["loss"] in ['mse', 'binary_crossentropy', 'categorical_crossentropy']:
+			dZ = A - Y
+		elif self.params["loss"] == 'mae':
+			dZ = np.sign(A - Y)
 		dA = self.layers[-1].backprop_output(dZ)
 		for i in range(len(layers)-2,-1,-1):
 			dA = layers[i].backprop(dA)

utils/gradient_check.py
@@ -65,7 +65,7 @@ def J(model, theta, X, Y):
     
     return cost+reg_loss
 
-def num_grad(J, model, theta, X, Y, epsilon = 1e-5):
+def num_grad(J, model, theta, X, Y, epsilon = 1e-7):
 
     dtheta = np.zeros(theta.shape)
     

utils/losses.py
@@ -9,7 +9,7 @@ def mae(y,yhat):
 
 def mse(y,yhat):
     m = y.shape[1]
-    return 1./m*np.sum((y-yhat)**2)
+    return 1./(2.*m)*np.sum((y-yhat)**2)
 
 def binary_crossentropy(y,yhat):
     m = y.shape[1]