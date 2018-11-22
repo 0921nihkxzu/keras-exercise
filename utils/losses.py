422d353a58c2174ae4fc890dbddc1e1226d68028
Markus O <orpana.markus@gmail.com>
2018-11-22 13:07:51 +0200

    modified losses--added clipping yhat to avoid log(0) or log(1); modified plotting in misc to work for categorical correctly; tested MNIST with fully connected in convolution.ipynb

--- utils/losses.py
+++ utils/losses.py
@@ -4,17 +4,20 @@
 import numpy as np
 
 def mae(y,yhat):
-    m = y.shape[1]
-    return 1./m*np.sum(np.abs(y-yhat))
+	m = y.shape[1]
+	return 1./m*np.sum(np.abs(y-yhat))
 
 def mse(y,yhat):
-    m = y.shape[1]
-    return 1./(2.*m)*np.sum((y-yhat)**2)
+	m = y.shape[1]
+	return 1./(2.*m)*np.sum((y-yhat)**2)
 
-def binary_crossentropy(y,yhat):
-    m = y.shape[1]
-    return -1./m*np.sum((y*np.log(yhat) + (1-y)*np.log(1-yhat)))
 
-def categorical_crossentropy(y,yhat):
-    m = y.shape[1]
-    return -1./m*np.sum(y*np.log(yhat))
+def binary_crossentropy(y,yhat,eps=1e-15):
+	m = y.shape[1]
+	yhat = np.clip(yhat,eps,1-eps)
+	return -1./m*np.sum((y*np.log(yhat) + (1-y)*np.log(1-yhat)))
+
+def categorical_crossentropy(y,yhat,eps=1e-15):
+	m = y.shape[1]
+	yhat = np.clip(yhat,eps,1-eps)
+	return -1./m*np.sum(y*np.log(yhat))
--- utils/misc.py
+++ utils/misc.py
@@ -4,10 +4,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_training_loss(model, mode='epoch'): # or mode = 'batch'
+def plot_training_loss(model, loss='binary_crossentropy', mode='epoch'): # or mode = 'batch'
+	
 	losses = getattr(model, mode+"_metrics")
 	iters = len(losses)
+	
+	if loss == 'binary_crossentropy':
+		getloss = lambda i: losses[i]['loss_tot']
+	elif loss == 'categorical_crossentropy':
+		getloss = lambda i: losses[i][-1]['loss_tot']
+
 	loss = np.zeros((1,iters))
 	for i in range(iters):
-		loss[0,i] = losses[i]['loss_tot']
+		loss[0,i] = getloss(i)
+
 	plt.plot(loss.T)
